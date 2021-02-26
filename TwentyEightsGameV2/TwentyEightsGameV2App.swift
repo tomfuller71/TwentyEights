674db63dd7fbef51e14aa3d775bbc6a7e79372bc
tomfuller71 <tomfuller71@gmail.com>
2021-02-26 17:39:33 -0500

    Initial Commit

---
+++ TwentyEightsGameV2/TwentyEightsGameV2App.swift
@@ -0,0 +1,17 @@
+//
+//  TwentyEightsGameV2App.swift
+//  TwentyEightsGameV2
+//
+//  Created by Thomas Fuller on 2/26/21.
+//
+
+import SwiftUI
+
+@main
+struct TwentyEightsGameV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
